690d80930311cdfdd13ef732e9eae22a10461dc2
Jingjing Duan <jduan@fitbit.com>
2016-12-11 10:12:09 -0800

    2016 day 1

---
+++ src/Y2016Day1.hs
@@ -0,0 +1,88 @@
+module Y2016Day1 where
+
+import Data.List.Split
+
+input =
+  "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1"
+
+data Direction
+  = North
+  | South
+  | East
+  | West
+  deriving (Show, Eq)
+
+nextDirection :: Direction -> Turn -> Direction
+nextDirection North L = West
+nextDirection North R = East
+nextDirection South L = East
+nextDirection South R = West
+nextDirection West L = South
+nextDirection West R = North
+nextDirection East L = North
+nextDirection East R = South
+
+data Turn
+  = L
+  | R
+  deriving (Show, Eq)
+
+type Distance = Int
+
+type X = Int
+
+type Y = Int
+
+data Coordinate =
+  Coordinate X
+             Y
+  deriving (Show, Eq)
+
+-- Coordinate And Direction
+data CAD =
+  CAD Coordinate
+      Direction
+
+blocksAway :: Coordinate -> Int
+blocksAway (Coordinate x y) = abs x + abs y
+
+getDirection 'L' = L
+getDirection 'R' = R
+getDirection _ = error "Invalid turn!"
+
+data Move =
+  Move Turn
+       Distance
+  deriving (Show, Eq)
+
+getMoves = map (\(dir:dis) -> Move (getDirection dir) (read dis :: Int))
+
+calculateBlocks line =
+  let (CAD coord dir) = calculate
+  in coord
+  where
+    parts = splitOn ", " line
+    moves = reverse $ getMoves parts
+    calculate =
+      foldr
+        (\(Move turn distance) (CAD (Coordinate x y) direction) ->
+            let nextDir = nextDirection direction turn
+            in case nextDir of
+                 North -> CAD (Coordinate x (y + distance)) nextDir
+                 South -> CAD (Coordinate x (y - distance)) nextDir
+                 East -> CAD (Coordinate (x + distance) y) nextDir
+                 West -> CAD (Coordinate (x - distance) y) nextDir)
+        (CAD (Coordinate 0 0) North)
+        moves
+
+-- part two
+data Segment =
+  Segment Coordinate
+          Coordinate
+
+-- areSegmentsOverlapped :: Segment -> Segment -> Maybe Coordinate
+-- areSegmentsOverlapped (Segment coord1 coord2) (Segment coor)
+main :: IO ()
+main =
+  let coord = calculateBlocks input
+  in print $ blocksAway coord
