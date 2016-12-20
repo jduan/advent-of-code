60637dbd609e323857a4e9bf0c0761625b945c5c
Jingjing Duan <jduan@fitbit.com>
2016-12-20 10:12:36 -0800

    refactoring

--- src/Y2016/Day3.hs
+++ src/Y2016/Day3.hs
@@ -1,13 +1,19 @@
 module Y2016.Day3 where
 
--- Check if 3 integers can form a valid triangle
-isTriangle
-  :: (Ord a, Num a)
-  => a -> a -> a -> Bool
-isTriangle a b c = a + b > c && a + c > b && b + c > a
+data Triangle =
+  Triangle Int
+           Int
+           Int
 
-toInts :: [String] -> [Int]
-toInts = map (\word -> read word :: Int)
+isValidTriangle :: Triangle -> Bool
+isValidTriangle (Triangle a b c) = a + b > c && a + c > b && b + c > a
+
+-- Turn a list of strings into a Triangle
+toTriangle :: [String] -> Triangle
+toTriangle [a, b, c] = Triangle (read a :: Int) (read b :: Int) (read c :: Int)
+
+-- Turn a list of list of strings into a list of Triangles
+toTriangles = map toTriangle
 
 -- Given a list of list of ints, count how many valid triangles there are.
 -- Example input:
@@ -16,32 +22,32 @@ toInts = map (\word -> read word :: Int)
 --   ["102", "302", "502"],
 -- ]
 countValidTriangles :: [[String]] -> Int
-countValidTriangles lines = sum counts
-  where
-    listOfInts = map toInts lines
-    counts :: [Int]
-    counts =
-      map
-        (\[a, b, c] ->
-            if isTriangle a b c
-              then 1
-              else 0)
-        listOfInts
+countValidTriangles lines = length $ filter isValidTriangle $ toTriangles lines
 
 -- Part 2
 countValidTriangles2 :: [[String]] -> Int
-countValidTriangles2 lines = count listOfInts
+countValidTriangles2 lines = count $ toTriangles lines
   where
-    listOfInts = map toInts lines
-    count (line1:line2:line3:rest) = count3Lines line1 line2 line3 + count rest
+    count (tri1:tri2:tri3:rest) = count3Tris tri1 tri2 tri3 + count rest
     count [] = 0
-    count3Lines [a1, b1, c1] [a2, b2, c2] [a3, b3, c3] =
-      sum $
-      map
-        (\bool ->
-            if bool
-              then 1
-              else 0)
-        bools
+    -- if the input isn't a multiple of 3, ignore the remaining lines
+    count _ = 0
+    count3Tris (Triangle a1 b1 c1) (Triangle a2 b2 c2) (Triangle a3 b3 c3) =
+      length $ filter (== True) bools
       where
-        bools = [isTriangle a1 a2 a3, isTriangle b1 b2 b3, isTriangle c1 c2 c3]
+        bools =
+          [ isValidTriangle (Triangle a1 a2 a3)
+          , isValidTriangle (Triangle b1 b2 b3)
+          , isValidTriangle (Triangle c1 c2 c3)
+          ]
+
+    
+-- Transform the input first, then call countValidTriangles
+countValidTriangles2' :: [[String]] -> Int
+countValidTriangles2' lines = countValidTriangles $ transform lines
+  where
+    transform [] = []
+    transform ([a1, b1, c1]:[a2, b2, c2]:[a3, b3, c3]:rest) =
+      [a1, a2, a3] : [b1, b2, b3] : [c1, c2, c3] : transform rest
+    -- if the input isn't a multiple of 3, ignore the remaining lines
+    transform a = []
--- test/Y2016/Day3Spec.hs
+++ test/Y2016/Day3Spec.hs
@@ -16,5 +16,10 @@ spec = do
       let tris = map words indata
       countValidTriangles2 tris `shouldBe` 1577
 
+    it "countValidTriangles2' should work" $ do
+      indata <- lines <$> readFile "test/Y2016/day3_input.txt"
+      let tris = map words indata
+      countValidTriangles2' tris `shouldBe` 1577
+
 main :: IO ()
 main = hspec spec
