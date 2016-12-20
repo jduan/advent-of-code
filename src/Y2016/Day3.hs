e89b6da44337097ee44dd22c92bd051340d95113
Jingjing Duan <jduan@fitbit.com>
2016-12-20 09:58:41 -0800

    Day 3 part 2

--- src/Y2016/Day3.hs
+++ src/Y2016/Day3.hs
@@ -6,6 +6,9 @@ isTriangle
   => a -> a -> a -> Bool
 isTriangle a b c = a + b > c && a + c > b && b + c > a
 
+toInts :: [String] -> [Int]
+toInts = map (\word -> read word :: Int)
+
 -- Given a list of list of ints, count how many valid triangles there are.
 -- Example input:
 -- [
@@ -15,9 +18,7 @@ isTriangle a b c = a + b > c && a + c > b && b + c > a
 countValidTriangles :: [[String]] -> Int
 countValidTriangles lines = sum counts
   where
-    list_of_ints = map to_ints lines
-    to_ints :: [String] -> [Int]
-    to_ints = map (\word -> read word :: Int)
+    listOfInts = map toInts lines
     counts :: [Int]
     counts =
       map
@@ -25,4 +26,22 @@ countValidTriangles lines = sum counts
             if isTriangle a b c
               then 1
               else 0)
-        list_of_ints
+        listOfInts
+
+-- Part 2
+countValidTriangles2 :: [[String]] -> Int
+countValidTriangles2 lines = count listOfInts
+  where
+    listOfInts = map toInts lines
+    count (line1:line2:line3:rest) = count3Lines line1 line2 line3 + count rest
+    count [] = 0
+    count3Lines [a1, b1, c1] [a2, b2, c2] [a3, b3, c3] =
+      sum $
+      map
+        (\bool ->
+            if bool
+              then 1
+              else 0)
+        bools
+      where
+        bools = [isTriangle a1 a2 a3, isTriangle b1 b2 b3, isTriangle c1 c2 c3]
--- test/Y2016/Day3Spec.hs
+++ test/Y2016/Day3Spec.hs
@@ -11,5 +11,10 @@ spec = do
       let tris = map words indata
       countValidTriangles tris `shouldBe` 862
 
+    it "countValidTriangles2 should work" $ do
+      indata <- lines <$> readFile "test/Y2016/day3_input.txt"
+      let tris = map words indata
+      countValidTriangles2 tris `shouldBe` 1577
+
 main :: IO ()
 main = hspec spec
